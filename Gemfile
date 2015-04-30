38c7e7c92fa5f6c1020192d77147bdc647d16975
Micah Geisel <micah@botandrose.com>
2015-04-30 12:37:37 -0700

    lock to sprockets 2.x for now.

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in ember-emblem-template.gemspec
 gemspec
 
-gem 'sprockets', ENV['SPROCKETS_VERSION'] if ENV['SPROCKETS_VERSION']
+gem 'sprockets', ["~> 2.0", "< 3.0"]
--- lib/ember/emblem/template.rb
+++ lib/ember/emblem/template.rb
@@ -7,13 +7,72 @@ module Ember
     autoload :Config, 'ember/emblem/config'
     autoload :Helper, 'ember/emblem/helper'
 
-    case Sprockets::VERSION
-    when /\A2\./
-      autoload :Template, 'ember/emblem/templates/sprockets2'
-    when /\A3\./
-      autoload :Template, 'ember/emblem/templates/sprockets3'
-    else
-      raise "Unsupported sprockets version: #{Sprockets::VERSION}"
+    class Template < Tilt::Template
+      include Helper
+
+      class << self
+        def configure
+          yield config
+        end
+
+        def default_mime_type
+          'application/javascript'
+        end
+
+        def setup(env)
+          env.register_engine '.hbs', self
+          env.register_engine '.emblem', self
+        end
+
+        def config
+          @config ||= Config.new
+        end
+      end
+
+      def prepare; end
+
+      def evaluate(scope, locals, &block)
+        filename = scope.pathname.to_s
+
+        raw = emblem?(filename)
+
+        if raw
+          template = data
+        else
+          template = mustache_to_emblem(filename, data)
+        end
+
+        if config.precompile
+          if raw
+            template = precompile_emblem(template)
+          else
+            template = precompile_ember_emblem(template, config.ember_template)
+          end
+        else
+          if raw
+            template = compile_emblem(data)
+          else
+            template = compile_ember_emblem(template, config.ember_template)
+          end
+        end
+
+        case config.output_type
+        when :amd
+          target = amd_template_target(config.amd_namespace, scope.logical_path.split(".").first)
+
+          "define('#{target}', ['exports'], function(__exports__){ __exports__['default'] = #{template} });"
+        when :global
+          target = global_template_target(scope.logical_path, config)
+
+          "#{target} = #{template}\n"
+        else
+          raise "Unsupported `output_type`: #{config.output_type}"
+        end
+      end
+
+      def config
+        @config ||= self.class.config.dup
+      end
     end
   end
 end
