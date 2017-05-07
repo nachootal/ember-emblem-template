dc508eeb1b7473aefa724c6c327180a518db5adb
Nacho <nachootal@gmail.com>
2017-05-07 11:05:56 +0200

    Using assets definition in config.

--- lib/ember/emblem/rails.rb
+++ lib/ember/emblem/rails.rb
@@ -3,7 +3,7 @@ module Ember
     module Rails
       class Engine < ::Rails::Engine
         initializer "ember_rails.setup", after: :append_assets_path, group: :all do |app|
-          Ember::Emblem::Template.setup app.assets
+          Ember::Emblem::Template.setup(app.assets.nil? ? app.config.assets : app.assets)
         end
       end
     end
